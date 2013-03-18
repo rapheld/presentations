875ee1e071c1a771d05be9a39fa30d720c1a6dce
Nathan Rapheld <nathan@netheril.local>
2013-03-17 17:21:02 -0700

    adding single_page_apps presentation WIP

---
+++ single_page_apps/noted_demo/lib/seed.js
@@ -0,0 +1,38 @@
+(function(exports) {
+
+  var seedData = [
+        {
+          id: 1,
+          title: "Shopping list",
+          message: "milk, eggs, cereal, bread",
+          createdAt: '2012-06-25 (PT)'
+        },
+        {
+          id: 2,
+          title: "Books to read",
+          message: "War and Peace, Dune, Foundation",
+          createdAt: '2012-09-04 (PT)'
+        },
+        {
+          id: 3,
+          title: "Quotables",
+          message: "It is very simple to be happy, but it is very difficult to be simple. - Rabindranath Tagore\nIn theory, theory and practice are the same. In practice, they are not. - Albert Einstein",
+          createdAt: '2012-10-02 (PT)'
+        },
+        {
+          id: 4,
+          title: "Lincoln",
+          message: "Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.",
+          createdAt: '2012-10-09 (PT)'
+        },
+        {
+          id: 5,
+          title: "Fir Tree",
+          message: "Fir (Abies) is a genus of 48–55 species of evergreen coniferous tree in the family Pinaceae. It is found through much of North and Central America, Europe, Asia, and North Africa, occurring in mountains over most of the range. Firs are most closely related to the genus Cedrus (cedar); Douglas-firs are not true firs, being of the genus Pseudotsuga.",
+          createdAt: '2012-10-14 (PT)'
+        },
+      ];
+
+  exports.seedData = seedData;
+
+}(this));
